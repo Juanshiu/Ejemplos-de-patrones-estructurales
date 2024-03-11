215ba3073ac69eb9515f578694bf908d9b1dd693
Juan Diego Montenegro Segura <134904103+Juanshiu@users.noreply.github.com>
2024-03-11 10:47:45 -0500

    Ejemplo Bridge

---
+++ Composite/index.php
@@ -0,0 +1,44 @@
+<?php
+// Implementación de la implementación
+interface Implementacion {
+}
+public function operacionImplementacion(): string {
+    // Code implementation here
+}
+
+// Implementación concreta A
+class ImplementacionConcretaA implements Implementacion {
+    public function operacionImplementacion(): string {
+        return "ImplementacionConcretaA";
+    }
+}
+
+// Implementación concreta B
+class ImplementacionConcretaB implements Implementacion {
+    public function operacionImplementacion(): string {
+        return "ImplementacionConcretaB";
+    }
+}
+
+// Abstracción
+abstract class Abstraccion {
+    protected $implementacion;
+
+    public function __construct (Implementacion $implementacion) {
+        $this->implementacion = $implementacion;
+    }
+
+    abstract public function operacion(): string;
+}
+
+// Abstracción refinada
+class AbstraccionRefinada extends Abstraccion {
+    public function operacion(): string {
+        return "AbstraccionRefinada: " . $this->implementacion->operacionImplementacion();
+    }
+}
+
+// Uso
+$implementacionA = new ImplementacionConcretaA();
+$abstraccion = new AbstraccionRefinada($implementacionA);
+echo $abstraccion->operacion(); // Salida: AbstraccionRefinada: ImplementacionConcretaA
