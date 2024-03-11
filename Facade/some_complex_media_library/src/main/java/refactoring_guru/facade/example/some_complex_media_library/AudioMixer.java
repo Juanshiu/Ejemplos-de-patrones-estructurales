9803027a9456dd2fdec092a9e65e3e1b0ec374db
Juan Diego Montenegro Segura <134904103+Juanshiu@users.noreply.github.com>
2024-03-11 10:26:17 -0500

    Ejemplo de patron Facade

---
+++ Facade/some_complex_media_library/src/main/java/refactoring_guru/facade/example/some_complex_media_library/AudioMixer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactoring_guru.facade.example.some_complex_media_library;
+
+import java.io.File;
+
+public class AudioMixer {
+    public File fix(VideoFile result){
+        System.out.println("AudioMixer: fixing audio...");
+        return new File("tmp");
+    }
+}
---
+++ Facade/some_complex_media_library/src/main/java/refactoring_guru/facade/example/some_complex_media_library/MPEG4CompressionCodec.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactoring_guru.facade.example.some_complex_media_library;
+
+
+public class MPEG4CompressionCodec extends Codec {
+    public String type = "mp4";
+
+}
